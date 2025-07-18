7d32d766dcb9d927a0c1c3d9c7e0ba60c5cd1d4c
Alejandro Salas <asalasledezma16@gmail.com>
2025-07-18 00:15:08 -0400

    Add db env vars config parameters

---
+++ app/config.py
@@ -0,0 +1,28 @@
+from pydantic import PostgresDsn
+from pydantic_core import MultiHostUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
+    DB_HOST: str
+    DB_PORT: int = 5432
+    DB_USER: str
+    DB_PASSWORD: str = ""
+    DB_DATABASE: str = ""
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme="postgresql",
+            username=self.DB_USER,
+            password=self.DB_PASSWORD,
+            host=self.DB_HOST,
+            port=self.DB_PORT,
+            path=self.DB_DATABASE,
+        )
+
+
+settings = Settings()
