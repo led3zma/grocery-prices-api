8d00ee6160671f89e082764cf382b1dc09f063d2
Alejandro Salas <asalasledezma16@gmail.com>
2025-08-03 01:13:41 -0400

    Add initial implementation for stores

--- app/crud.py
+++ app/crud.py
@@ -1,8 +1,8 @@
-# Category CRUD
-
 from fastapi import HTTPException
 from sqlmodel import Session, select
-from app.models import Category, CategoryBase, CategoryUpdate
+from app.models import Category, CategoryBase, CategoryUpdate, Store, StoreBase, StoreUpdate
+
+# Category CRUD
 
 
 def create_category(category: CategoryBase, session: Session) -> Category:
@@ -37,5 +37,43 @@ def delete_category(category_id: int, session: Session) -> bool:
     if not category_db:
         raise HTTPException(status_code=404, detail="Category not found")
     session.delete(category_db)
+    session.commit()
+    return True
+
+
+# Store CRUD
+
+def create_store(store: StoreBase, session: Session) -> Store:
+    store_db = Store.model_validate(store)
+    session.add(store_db)
+    session.commit()
+    session.refresh(store_db)
+    return store_db
+
+
+def read_store(store_id: int, session: Session) -> Store | None:
+    return session.get(Store, store_id)
+
+
+def read_stores(session: Session) -> list[Store]:
+    return session.exec(select(Store)).all()
+
+
+def update_store(store_id: int, store: StoreUpdate, session: Session) -> Store:
+    store_db = read_store(store_id, session)
+    if not store_db:
+        raise HTTPException(status_code=404, detail="Store not found")
+    store_db.sqlmodel_update(store.model_dump(exclude_unset=True))
+    session.add(store_db)
+    session.commit()
+    session.refresh(store_db)
+    return store_db
+
+
+def delete_store(store_id: int, session: Session) -> bool:
+    store_db = read_store(store_id, session)
+    if not store_db:
+        raise HTTPException(status_code=404, detail="Store not found")
+    session.delete(store_db)
     session.commit()
     return True
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from app.database import SessionDep
-from app.models import Category, CategoryBase
+from app.models import Category, CategoryBase, CategoryUpdate, Store, StoreBase, StoreUpdate
 import app.crud as crud
 
 from fastapi import APIRouter, Body, Path
@@ -38,4 +38,36 @@ async def delete_category(category_id: Annotated[int, Path(gt=0)], session: Sess
         return {'result': 'False'}
     return {'result': 'ok'}
 
+# Store Router
+
+store_router = APIRouter(prefix='/stores', tags=['store'])
+
+
+@store_router.get('/')
+async def read_stores(session: SessionDep) -> list[Store]:
+    return crud.read_stores(session)
+
+
+@store_router.get('/{store_id}')
+async def read_store(store_id: Annotated[int, Path(gt=0)], session: SessionDep) -> Store:
+    return crud.read_store(store_id, session)
+
+
+@store_router.post('/')
+async def create_store(store: Annotated[StoreBase, Body()], session: SessionDep) -> Store:
+    return crud.create_store(store, session)
+
+
+@store_router.put('/{store_id}')
+async def update_store(store_id: Annotated[int, Path(gt=0)], store: Annotated[StoreUpdate, Body()], session: SessionDep) -> Store:
+    return crud.update_store(store_id, store, session)
+
+
+@store_router.delete('/{store_id}')
+async def delete_store(store_id: Annotated[int, Path(gt=0)], session: SessionDep) -> dict:
+    if not crud.delete_store(store_id, session):
+        return {'result': 'False'}
+    return {'result': 'ok'}
+
 router.include_router(category_router)
+router.include_router(store_router)
